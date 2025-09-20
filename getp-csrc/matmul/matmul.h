529090f37744124ea636c8ef3ddae303d2e44ac5
Tuan Phan <tuan.phan@moreh.com.vn>
2025-09-20 09:31:42 +0000

    moe bucketing

--- getp-csrc/matmul/matmul.h
+++ getp-csrc/matmul/matmul.h
@@ -12,24 +12,6 @@ __global__ void matmul_bias_gemm_kernel_float(
     float *out, const float *x, const float *w, const float *b,
     int H, int D, int batch_size, const int *pos);
 
-__global__ void mlp1_matmul_bias_kernel(
-    float *out, const float *x, const bf16_t *w, const float *b,
-    const int *topk_i, int layer, int E, int H, int IM,
-    int B, const int *pos);
-
-__global__ void mlp1_split_swiglu_kernel(
-    float *gate_up_out, const float *gate_up_raw, float clip,
-    int IM, int B, int K, const int *pos);
-
-__global__ void mlp2_matmul_bias_kernel(
-    float *out, const float *gate_up, const bf16_t *w, const float *b,
-    const int *topk_i, int layer, int E, int IM, int H, int B,
-    const int *pos);
-
-__global__ void mlp2_weighted_accum_kernel(
-    float *e_agg, const float *mlp2_out, const float *topk_v,
-    int H, int B, int K, const int *pos);
-
 __global__ void moe_count_assignments_kernel(int *counts,
                                              const int *topk_i,
                                              int total_assignments,
